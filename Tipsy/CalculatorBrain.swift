0c9dccaacf44d9a932fd5460ab580dec5afb4fca
gabrielCastilloSerafim <castillo.serafim.gabriel@gmail.com>
2022-08-25 09:05:09 +0200

    add git ignore file

---
+++ Tipsy/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Tipsy
+//
+//  Created by Gabriel Castillo Serafim on 24/8/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Tipsy/Controler/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Gabriel Castillo Serafim on 24/8/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
