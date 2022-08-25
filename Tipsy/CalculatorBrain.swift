a4186852861399f9b0da3bf6b4cbc0b1e1037067
gabrielCastilloSerafim <castillo.serafim.gabriel@gmail.com>
2022-08-25 09:05:35 +0200

    Final project

--- Tipsy/CalculatorBrain.swift
+++ Tipsy/CalculatorBrain.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2022 The App Brewery. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+struct CalculatorBrain {
+    
+    var totalPerPerson:Float = 0
+    var calculationExplained:String = ""
+    
+    mutating func performCalculation(numberInputted:Float, percentageSelected:Float, peopleToSplit:Float) {
+        //Calcula numero
+        totalPerPerson = numberInputted / peopleToSplit + (numberInputted * percentageSelected / peopleToSplit)
+        
+        //Cria instruções
+        let peopleNumber = String(format: "%.0f", peopleToSplit)
+        let percentage = String(format: "%.0f", percentageSelected*100)
+        
+        calculationExplained = "Split between \(peopleNumber) people, with \(percentage)% tip."
+    }
+    
+}
--- Tipsy/Controler/CalculatorViewController.swift
+++ Tipsy/Controler/CalculatorViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    @IBOutlet weak var zeroPercentButton: UIButton!
+    @IBOutlet weak var tenPercentButton: UIButton!
+    @IBOutlet weak var twentyPercentButton: UIButton!
+    
+    var calculatorBrain = CalculatorBrain()
+    
+    var percentageSelected:Float = 0.1
+    
+    @IBAction func percentageChanged(_ sender: UIButton) {
+        zeroPercentButton.isSelected = false
+        tenPercentButton.isSelected = false
+        twentyPercentButton.isSelected = false
+        sender.isSelected = true
+        
+        //DismissKeyboard
+        textInput.endEditing(true)
+        
+        percentageSelected = Float(sender.tag)/100
     }
-
-
+    
+    @IBAction func stepperPressed(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        //Capturar o valor do numero introduzido
+        let numberInputted = Float(textInput.text ?? "0")
+        
+        //Capturar valor do numero de pessoas a dividir
+        let peopleToSplit = Float(splitNumberLabel.text!)
+        
+        calculatorBrain.performCalculation(numberInputted: numberInputted ?? 0, percentageSelected: percentageSelected, peopleToSplit: peopleToSplit!)
+        
+        self.performSegue(withIdentifier: "goToResultPage", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "goToResultPage" {
+            let nextPage = segue.destination as! ResultViewController
+            
+            nextPage.value = calculatorBrain.totalPerPerson
+            nextPage.calcInstructions = calculatorBrain.calculationExplained
+        }
+    }
+    
 }
 
